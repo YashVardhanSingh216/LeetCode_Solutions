55d5643b4c1e3ded61f549f4ecf24c16b56a394a
YashVardhanSingh216 <yashvardhansingh216@gmail.com>
2026-09-17 23:37:15 +0530

    Update 645. Set Mismatch.cpp

--- Arrays/645. Set Mismatch.cpp
+++ Arrays/645. Set Mismatch.cpp
@@ -29,6 +29,33 @@ public:
         ans.push_back(left);
         
         return ans;
+
+        // BEST OPTIMAL APPROACH :-
+        
+        // The values itself tells which index values are present
+        int duplicate = 0;
+        for(int i=0; i<n; i++){
+
+            if(nums[abs(nums[i]) - 1] < 0)
+            duplicate = abs(nums[i]);
+
+            else
+            nums[abs(nums[i]) - 1] *= -1;
+
+        }
+
+        int missing = 0;
+
+        for(int i=0; i<n; i++){
+
+            if(nums[i] > 0){
+            missing = i+1;
+            break;
+            }
+        } 
+
+        return vector<int>{duplicate,missing};     
+
         
     }
 };
