85ab3e1a5fbf0365b9f73e6a7c6de5830d0eff52
YashVardhanSingh216 <yashvardhansingh216@gmail.com>
2026-09-20 22:24:04 +0530

    Create 3498. Reverse Degree of a String.cpp

---
+++ Strings/3498. Reverse Degree of a String.cpp
@@ -0,0 +1,16 @@
+// 3498. Reverse Degree of a String
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        
+        int n = s.size();
+        int sum = 0;
+
+        for(int i=0; i<n; i++){
+            sum += (26 - (s[i] - 'a'))*(i+1);
+        }
+
+        return sum;
+    }
+};
